529e434b5b9a3e21cffeb8f51f803c56e99021cd
BlockA <34108858+Nareleth@users.noreply.github.com>
2025-07-23 10:39:33 +0000

    1.1 (#4)
    * Fixed canvas height and width
    
    * Updated README
    
    * Grid example
    
    * Updated README
    
    * Updated version number
    
    ---------
    
    Co-authored-by: blocka <blocka@amenthes.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
 	name="svgcanvas",
-	version="1.0",
+	version="1.1",
 	description="Python SVG File Writer",
 	author="blocka",
 	packages=find_packages(),
